826eee64ab94d732f5d3601234aa4291dece4c2f
toigamo <89833524+toigamo@users.noreply.github.com>
2021-09-03 19:39:32 +0700

    Update win7-vnc.sh

--- win7-vnc.sh
+++ win7-vnc.sh
@@ -7,7 +7,7 @@ echo Please wait for installing...
 echo "Installing QEMU (2-3m)..."
 sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Downloading Windows Disk...
-curl -L -o lite7.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.3/providers/qemu.box
+curl -L -o lite7.qcow2 https://app.vagrantup.com/toigamo/boxes/windows/versions/0.0.1/providers/qemu.box
 echo "Windows 7 x86 Lite On Google Colab"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
